0ab6e51c512a72670ad5c664731ab82266ae66bf
Aleck <aleckg.valdez@gmail.com>
2022-10-13 22:01:01 +0800

    g my g

---
+++ actions/getdata.php
@@ -0,0 +1,42 @@
+<?php
+include 'DbConnect.php';
+
+
+function getFirstName(string $uid) {
+	$db = new DbConnect;
+	$conn = $db->connect();
+	
+	$stmt = $conn->prepare("SELECT tblusers.firstname AS firstname FROM tblusers WHERE id = :uid");
+	$stmt->execute([
+		':uid' => $uid
+	]);
+	$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($users as $user) {
+		$firstname = $user['firstname'];
+	}
+	return $firstname;
+}
+
+function getId(string $uid) {
+	$db = new DbConnect;
+	$conn = $db->connect();
+	
+	$stmt = $conn->prepare("SELECT tblusers.id AS userid FROM tblusers WHERE id = :uid");
+	$stmt->execute([
+		':uid' => $uid
+	]);
+	$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($users as $user) {
+		$id = $user['userid'];
+	}
+	return $id;
+}
+
+
+getFirstName('U-0000001');
+getId('U-0000001');
+
+
+
+
+ ?>
